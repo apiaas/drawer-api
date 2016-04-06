31270f035d8666b37aabdc442954c062410153dc
Sergey <serjpavlenko@mail.ru>
2016-04-06 12:32:04 +0300

    rest framework was added to django

---
+++ api/local_settings.py
@@ -0,0 +1,21 @@
+##################
+# REST FRAMEWORK #
+##################
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'api.authentication.TokenAuthentication'
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter'
+    ),
+    'PAGINATE_BY': 25,
+    'PAGINATE_BY_PARAM': 'paginate_by',
+}
