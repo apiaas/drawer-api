423f0aa99b5875ebec83793a28b2b22cbd263d70
Sergey <serjpavlenko@mail.ru>
2016-04-10 15:07:05 +0300

    url warnings was fixed

--- apiauth/urls.py
+++ apiauth/urls.py
@@ -1,12 +1,25 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.views import logout
+from apiauth.views import apilogin, obtain_auth_token
 
 app = '^api/'
 
-urlpatterns = patterns('apiauth.views',
-    url(app + 'login/$', 'apilogin', name='api_login'),
+urlpatterns = [
+    url(app + 'login/$', apilogin, name='api_login'),
     url(app + 'logout/$', logout, name='api_logout'),
-    url(app + 'auth-token/', 'obtain_auth_token', name='api-token-auth'),
+    url(app + 'auth-token/', obtain_auth_token, name='api-token-auth'),
     #url(app + 'auth-token-portal/', 'auth_token_portal', name='auth-token-portal'),
     #url(app + 'docs/', 'documentation', name='api-documentation'),
-)
+]
+#
+# urlpatterns = [
+#     url(r'^', include(router.urls)),
+#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+#
+#     # client
+#     url(r'', include('client.urls')),
+#     #apilogin
+#     url(r'', include('apiauth.urls')),
+#
+#     url(r'^admin/', admin.site.urls),
+# ]
--- client/urls.py
+++ client/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, url
+from client.views import client_detail, client_list
 
 
-urlpatterns = patterns(
-    'client.views',
+urlpatterns = [
+    # 'client.views',
 
     # client
-    url('api/client/client/$', 'client_list', name='api_client_list'),
-    url('api/client/client/(?P<pk>\d+)/$', 'client_detail',
+    url('api/client/client/$', client_list, name='api_client_list'),
+    url('api/client/client/(?P<pk>\d+)/$', client_detail,
         name='api_client_detail'),
-)
+]
